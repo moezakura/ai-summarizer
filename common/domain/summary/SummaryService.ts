072417dfd633ce0c23b5aa985c5b880a3bc8e8ec
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2024-06-09 01:38:31 +0900

    Pupetterが落ちないようにした

--- common/domain/summary/SummaryService.ts
+++ common/domain/summary/SummaryService.ts
@@ -46,31 +46,69 @@ export class SummaryService {
 
 	async summarize(item: SummaryItem): Promise<string> {
 		const ollamaLlm = this.ollamaLLM;
-
 		const page = await this.browser!.newPage();
-		await page.goto(item.uri);
-		const html = await page.evaluate(() => {
-			return document.documentElement.outerHTML;
-		});
 
-		const dom = new JSDOM(html);
-		const content = new Readability(dom.window.document).parse();
+		try {
+			await page.goto(item.uri);
+			const html = await page.evaluate(() => {
+				return document.documentElement.outerHTML;
+			});
 
-		// const transformer = new MozillaReadabilityTransformer();
-		const splitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
-			chunkSize: 6000,
-			chunkOverlap: 100,
-		});
-		if (!content?.content) {
-			throw new Error(`failed to get content: ${JSON.stringify(content)}`);
-		}
+			const dom = new JSDOM(html);
+			const content = new Readability(dom.window.document).parse();
 
-		const newDocuments = await splitter.splitText(content!.content);
+			// const transformer = new MozillaReadabilityTransformer();
+			const splitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
+				chunkSize: 6000,
+				chunkOverlap: 100,
+			});
+			if (!content?.content) {
+				throw new Error(`failed to get content: ${JSON.stringify(content)}`);
+			}
 
-		const results: string[] = [];
+			const newDocuments = await splitter.splitText(content!.content);
 
-		for (const doc of newDocuments) {
-			const result = await retryCall(async () => {
+			const results: string[] = [];
+
+			for (const doc of newDocuments) {
+				const result = await retryCall(async () => {
+					const outputParser = new StringOutputParser();
+
+					const summaryPrompt = PromptTemplate.fromTemplate(
+						[
+							"あなたはWebページの要約の専門家です。\n",
+							"あなたの目的はタイトルとWebページのコンテンツから読みやすく要約した文章を作ることです。\n",
+							"以下はWebページから抽出した文章です。\n",
+							"---------------------\n",
+							"Webページのタイトル: {title} \n",
+							"Webページのコンテンツ:\n{page_content}\n",
+							"---------------------\n",
+							"提供された情報のみを要約に含めてください。\n",
+							"記事の内容を箇条書きで要約してください。箇条書きの先頭には意味に合う絵文字をつけてください。\n",
+							"元のWebページに記載されていない情報を補完してはいけません。\n",
+							"必ず日本語で教えてください。\n",
+						].join(),
+					);
+
+					const chain = summaryPrompt.pipe(ollamaLlm).pipe(outputParser);
+
+					return await chain.invoke({
+						title: item.title,
+						page_content: doc,
+					});
+				}, 5);
+
+				console.warn(result);
+
+				results.push(result);
+			}
+
+			const mergedResult = results.join("\n");
+			if (mergedResult.length < 500) {
+				return mergedResult;
+			}
+
+			const complessSummaryText = await retryCall(async () => {
 				const outputParser = new StringOutputParser();
 
 				const summaryPrompt = PromptTemplate.fromTemplate(
@@ -82,6 +120,7 @@ export class SummaryService {
 						"Webページのタイトル: {title} \n",
 						"Webページのコンテンツ:\n{page_content}\n",
 						"---------------------\n",
+						"文体が統一されていない場合は、文体を統一してください。\n",
 						"提供された情報のみを要約に含めてください。\n",
 						"記事の内容を箇条書きで要約してください。箇条書きの先頭には意味に合う絵文字をつけてください。\n",
 						"元のWebページに記載されていない情報を補完してはいけません。\n",
@@ -92,49 +131,17 @@ export class SummaryService {
 				const chain = summaryPrompt.pipe(ollamaLlm).pipe(outputParser);
 
 				return await chain.invoke({
-					title: item.title,
-					page_content: doc,
+					title: item.title ?? "不明なタイトル",
+					page_content: mergedResult,
 				});
 			}, 5);
 
-			console.warn(result);
-
-			results.push(result);
+			return complessSummaryText;
+		} catch (e) {
+			throw e;
+		} finally {
+			// 後片付け
+			await page.close();
 		}
-
-		const mergedResult = results.join("\n");
-		if (mergedResult.length < 500) {
-			return mergedResult;
-		}
-
-		const complessSummaryText = await retryCall(async () => {
-			const outputParser = new StringOutputParser();
-
-			const summaryPrompt = PromptTemplate.fromTemplate(
-				[
-					"あなたはWebページの要約の専門家です。\n",
-					"あなたの目的はタイトルとWebページのコンテンツから読みやすく要約した文章を作ることです。\n",
-					"以下はWebページから抽出した文章です。\n",
-					"---------------------\n",
-					"Webページのタイトル: {title} \n",
-					"Webページのコンテンツ:\n{page_content}\n",
-					"---------------------\n",
-					"文体が統一されていない場合は、文体を統一してください。\n",
-					"提供された情報のみを要約に含めてください。\n",
-					"記事の内容を箇条書きで要約してください。箇条書きの先頭には意味に合う絵文字をつけてください。\n",
-					"元のWebページに記載されていない情報を補完してはいけません。\n",
-					"必ず日本語で教えてください。\n",
-				].join(),
-			);
-
-			const chain = summaryPrompt.pipe(ollamaLlm).pipe(outputParser);
-
-			return await chain.invoke({
-				title: item.title ?? "不明なタイトル",
-				page_content: mergedResult,
-			});
-		}, 5);
-
-		return complessSummaryText;
 	}
 }
